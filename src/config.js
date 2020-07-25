c2f055de9aad6a1da1cc92046bcf4939fe2be796
Leon Marbukh <leonmarbukh@gmail.com>
2020-07-25 07:10:44 -0700

    added api to end of url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
  export default {
-   API_ENDPOINT: `https://boiling-springs-79266.herokuapp.com`,
+   API_ENDPOINT: `https://boiling-springs-79266.herokuapp.com/api`,
    //API_ENDPOINT: `http://localhost:8000/api`,
    API_KEY: process.env.REACT_APP_API_KEY,
    TOKEN_KEY: "newsful-client-auth-token",
