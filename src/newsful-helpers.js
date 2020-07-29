4438f8b7ff9dca36421fa1fb261d3c6cc510a5c1
Leon Marbukh <leonmarbukh@gmail.com>
2020-07-29 15:24:41 -0700

    fixed npr fetch

--- src/newsful-helpers.js
+++ src/newsful-helpers.js
@@ -12,7 +12,7 @@
 export const conserv_search_news =
          "https://api.currentsapi.services/v1/search?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=foxnews.com,breitbart.com&keywords=";
 export const neutral_search_news =
-         "https://api.currentsapi.services/v1/search?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=bbc.com,npr.com&keywords=";
+         "https://api.currentsapi.services/v1/search?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=bbc.com,npr.org&keywords=";
 
 export const liberal_search_news =
          "https://api.currentsapi.services/v1/search?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=msnbc.com,huffpost.com&type=1&keywords=";
@@ -21,6 +21,6 @@ export const conserv_popular_news =
   "https://api.currentsapi.services/v1/latest-news?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=foxnews.com,breitbart.com"; 
 
 export const neutral_popular_news =
-         "https://api.currentsapi.services/v1/latest-news?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=bbc.com,npr.com"; 
+         "https://api.currentsapi.services/v1/latest-news?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=bbc.com,npr.org"; 
 export const liberal_popular_news =
          "https://api.currentsapi.services/v1/latest-news?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=msnbc.com,huffpost.com&type=1"; 
