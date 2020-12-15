7aaec3bb1621c9e4232a4158a26dfd80914ea9bf
lyunya <50932089+lyunya@users.noreply.github.com>
2020-12-14 21:06:46 -0800

    Update newsful-helpers.js

--- src/newsful-helpers.js
+++ src/newsful-helpers.js
@@ -1,14 +1,3 @@
-// export const popular_news =
-//          "https://newsapi.org/v2/top-headlines?apiKey=fcea81b72a8041cb91c36892e482ab0d&sources=breitbart-news,fox-news,national-review,abc-news,bbc-news,usa-today,msnbc,the-huffington-post,cnn&pageSize=33";
-// export const search_news =
-//          "https://newsapi.org/v2/everything?sources=msnbc,the-huffington-post,cnn,abc-news,bbc-news,usa-today,breitbart-news,fox-news,national-review&apiKey=fcea81b72a8041cb91c36892e482ab0d&language=en&sortBy=publishedAt&pageSize=33&q=";
-
-// export const new_popular_news =
-//          "https://api.currentsapi.services/v1/latest-news?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=msnbc.com,cnn.com,foxnews.com,huffpost.com,abcnews.go.com,apnews.com,usatoday.com,breitbart.com,nationalreview.com"; 
-         
-// export const new_search_news =
-//          "https://api.currentsapi.services/v1/search?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=msnbc.com,cnn.com,foxnews.com,huffpost.com,abcnews.go.com,apnews.com,usatoday.com,breitbart.com,nationalreview.com&keywords=";
-
 export const conserv_search_news =
          "https://api.currentsapi.services/v1/search?apiKey=U-KpTJ3lU2QW7BjPvER-ArREpL9Le5wHQ-KdFLco52cBbJe7&domain=foxnews.com,breitbart.com&keywords=";
 export const neutral_search_news =
