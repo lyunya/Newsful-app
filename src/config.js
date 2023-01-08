17cee5daa32e8001d978c5b785864fedbb74a1be
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-07 17:48:21 -0800

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  API_ENDPOINT: `https://newsful-api.onrender.com`,
+  API_ENDPOINT: `https://newsful-api.onrender.com/api`,
   // API_ENDPOINT: `http://localhost:8000/api`,
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'newsful-client-auth-token',
